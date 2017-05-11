8b129115c095652bee92f590409885a060d600fa
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-05-11 15:20:21 +0100

    A test using Wood data

--- test.sh
+++ test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # 11-5-2017 MRC-Epid JHZ
 
+wget http://portals.broadinstitute.org/collaboration/giant/images/0/01/GIANT_HEIGHT_Wood_et_al_2014_publicrelease_HapMapCeuFreq.txt.gz
+gunzip -c GIANT_HEIGHT_Wood_et_al_2014_publicrelease_HapMapCeuFreq.txt.gz | \
 awk '{
   FS=OFS="\t";
   t=NR
   if(NR==1) print "SNP","A1","A2","Z"
   else print $1,$2,$3,$5/$6
-}' /genetics/data/twas/14-10-16/GIANT/GIANT_HEIGHT_Wood_et_al_2014_publicrelease_HapMapCeuFreq.txt \
-   | sort -k1,1 > height
+}' | sort -k1,1 > height
 
 ewas-fusion.sh height
-rm height
+rm GIANT_HEIGHT_Wood_et_al_2014_publicrelease_HapMapCeuFreq.txt.gz height
