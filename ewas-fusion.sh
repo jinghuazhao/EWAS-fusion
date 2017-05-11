ce655e6d9852799ec6cfde7b22244da19928d420
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-05-11 14:13:05 +0100

    EWAS fusion

--- ewas-fusion.sh
+++ ewas-fusion.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #11-5-2017 MRC-Epid JHZ
 
-#parallel '/bin/echo {1} {2} /genetics/bin/FUSION/GTEx/{1}' ::: $(/bin/cat ../GTEx.list) ::: $(seq 22)>../GTEx.runlist
-
 if [ $# -lt 1 ] || [ "$1" == "-h" ]; then
     echo "Usage: gtex-sge.sh <input>"
     echo "where <input> is in tab-delimited format:"
@@ -14,8 +12,27 @@ dir=$(pwd)/$(basename $1).tmp
 if [ ! -d $dir ]; then
    mkdir -p $dir
 fi
+# SNP   pos     A1      A2      Effect  se      N
+
 EWAS_fusion=/genetics/bin/EWAS-fusion
-ln -sf $WAS_fusion/glist-hg19 $dir/glist-hg19
+awk '(NR>1) {
+  FS=OFS="\t"
+  $3=toupper($3)
+  $4=toupper($4)
+  print $1, $2, $3, $4, $5/$6, $7
+}' $dir.txt | \
+sort -k1,1 | \
+join -12 -21 $EWAS_fusion/EWAS.bim - | \
+awk -f $EWAS_fusion/CLEAN_ZSCORES_N.awk | \
+awk '{if(NR==1) print "SNP","A1","A2","Z","N"; else {$2="";print}}' > $dir.input
+
 WGT=$EWAS_fusion/EWAS/
 LDREF=$EWAS_fusion/LDREF
-qsub -cwd -sync y -v EWAS_fusion=$EWAS_fusion -v dir=$dir -v sumstats=$(pwd)/$1 -v WGT=$WGT -v LDREF=$LDREF $EWAS_fusion/ewas-fusion.qsub
+ln -sf $EWAS_fusion/glist-hg19 $dir/glist-hg19
+FUSION=/genetics/bin/fusion_twas
+RSCRIPT=/genetics/data/software/bin/Rscript
+qsub -cwd -sync y \
+     -v EWAS_fusion=$EWAS_fusion -v dir=$dir -v sumstats=$(pwd)/$1 -v WGT=$WGT -v LDREF=$LDREF -v FUSION=$FUSION -v RSCRIPT=$RSCRIPT -vLOCUS_WIN=500000 \
+     $EWAS_fusion/ewas-fusion.qsub
+
+#parallel '/bin/echo {1} {2} /genetics/bin/FUSION/GTEx/{1}' ::: $(/bin/cat ../GTEx.list) ::: $(seq 22)>../GTEx.runlist
