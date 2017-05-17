1a2a268c36e20f747228b98117d206a4442dbb9f
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-05-17 23:53:42 +0100

    EWAS annotation

--- ewas-annotate.R
+++ ewas-annotate.R
@@ -6,6 +6,16 @@
 #' @author Alexia Cardona, \email{alexia.cardona@@mrc-epid.cam.ac.uk}
 #' @date May 2017
 
+EWAS_fusion <- "/genetics/bin/EWAS-fusion"
+infinium_humanmethylation450_beadchip_setup <- function()
+# also see /genetics/data/twas/doc
+{
+	hmv12 <-"ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/ProductFiles/HumanMethylation450/HumanMethylation450_15017482_v1-2.csv"
+	download.file(hmv12,"HumanMethylation450_15017482_v1-2.csv")
+	mhd <- "https://support.illumina.com/content/dam/illumina-support/documents/myillumina/4214cc3b-e52e-4ab4-a4d7-b956f62ed208/450k_manifest_header_descriptions.xlsx"
+	download.file(mhd,"450k_manifest_header_descriptions.xlsx")
+}
+
 args <- commandArgs(trailingOnly=TRUE)
 prefix <- args[1]
 prefix <- ifelse(substring(prefix,nchar(prefix)) != "/", paste(sep="", prefix, "/"), prefix)
@@ -16,7 +26,7 @@ if(is.na(prefix)) {
 	quit("yes")
 }
 
-#Message to users
+#Messages to users
 cat("EWAS-fusion Annotator - a utility to annotate results from the EWAS-fusion pipeline.\n")
 cat("by Alexia Carona, PhD\n\n")
 cat("For more information about FUSION and the headings in the output please visit\n\n")
@@ -32,22 +42,22 @@ for(i in 1:22)
 }
 
 #annotate files
-anno <- read.csv("/genetics/bin/EWAS-fusion/HumanMethylation450_15017482_v1-2.csv",as.is=TRUE, skip=7)
+anno <- read.csv(paste0(EWAS_fusion,"/HumanMethylation450_15017482_v1-2.csv"),as.is=TRUE, skip=7)
 colnames(temp)[2] <- "Name"
 annotated.data <- merge(temp, anno, by="Name")
-rm(temp) #free memory
+rm(temp)
 
 #sort by P-value
 sorted.data <- annotated.data[order(annotated.data$TWAS.P),]
 rm(annotated.data) #free memory
-write.csv(sorted.data, file=paste(sep="", prefix, "annotatedSorted.csv"), quote=FALSE, row.names=FALSE)
-cat(paste(sep="", "Annotated EWAS-fusion results for all chromosmes are in ", prefix, "annotatedSorted.csv\n"))
+write.csv(sorted.data, file=paste0(prefix, "annotatedSorted.csv"), quote=FALSE, row.names=FALSE)
+cat(paste0("Annotated EWAS-fusion results for all chromosmes are in ", prefix, "annotatedSorted.csv\n"))
 
 #get list of significant CpGS
 sig.data <- subset(sorted.data, TWAS.P < (0.05/nrow(sorted.data)))
-write.csv(sig.data, file=paste(sep="", prefix, "annotedSortedSignificant.csv"), quote=FALSE, row.names=FALSE)
-cat(paste(sep="", "Bonferroni significant list in ", prefix, "annotatedSortedSignificant.csv\n"))
-rm(sorted.data) #free memory
+write.csv(sig.data, file=paste0(prefix, "annotedSortedSignificant.csv"), quote=FALSE, row.names=FALSE)
+cat(paste0("Bonferroni significant list in ", prefix, "annotatedSortedSignificant.csv\n"))
+rm(sorted.data)
 rm(sig.data)
 
 
@@ -59,7 +69,7 @@ cat("Processing Joint/conditional analysis results\n\n")
 temp <- NULL
 for (i in 1:22)
 {
-	temp <- rbind(temp, read.table(paste(sep="", prefix, i, ".top.analysis.joint_included.dat"), header=TRUE))
+	temp <- rbind(temp, read.table(paste0(prefix, i, ".top.analysis.joint_included.dat"), header=TRUE))
 }
 
 #annotate files
@@ -68,15 +78,6 @@ annotated.data <- merge(temp, anno, by="Name")
 rm(temp) #free memory
 
 sorted.data <- annotated.data[order(annotated.data$JOINT.P),]
-write.csv(sorted.data, file=paste(sep="", prefix, "annotatedJoint_included.csv"), quote=FALSE, row.names=FALSE)
-cat(paste(sep="", "Annotated Joint/Conditional analysis results in ", prefix, "annotatedJoint_included.csv\n"))
+write.csv(sorted.data, file=paste0(prefix, "annotatedJoint_included.csv"), quote=FALSE, row.names=FALSE)
+cat(paste0("Annotated Joint/Conditional analysis results in ", prefix, "annotatedJoint_included.csv\n"))
 rm(sorted.data) #free memory
-
-infinium_humanmethylation450_beadchip_setup <- function()
-# also see /genetics/data/twas/doc
-{
-	hmv12 <-"ftp://webdata2:webdata2@ussd-ftp.illumina.com/downloads/ProductFiles/HumanMethylation450/HumanMethylation450_15017482_v1-2.csv"
-	download.file(hmv12,"HumanMethylation450_15017482_v1-2.csv")
-	mhd <- "https://support.illumina.com/content/dam/illumina-support/documents/myillumina/4214cc3b-e52e-4ab4-a4d7-b956f62ed208/450k_manifest_header_descriptions.xlsx"
-	download.file(mhd,"450k_manifest_header_descriptions.xlsx")
-}
