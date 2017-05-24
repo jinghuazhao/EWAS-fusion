4643f61cb8d37a8758d0bdc2801f36fc1a58c8f7
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-05-24 09:07:28 +0100

    EWAS-fusion annotation

--- ewas-annotate.R
+++ ewas-annotate.R
@@ -82,16 +82,16 @@ write.csv(sorted.data, file=paste0(prefix, "annotatedJoint_dropped.csv"), quote=
 cat(paste0("Conditional annotation: ", prefix, "annotatedJoint_dropped.csv\n"))
 
 annotated.data <- within(annotated.data, {TWAS.P.Bonferroni <- 0.05/N})
-included <- included[,!(names(included)%in%c("FILE","TWAS.Z","TWAS.P"))]
+included <- included[,setdiff(names(included),c("FILE","TWAS.Z","TWAS.P"))]
 aj <- merge(annotated.data, included, by="Name", all=TRUE)
 aj <- aj[with(aj,order(CHR,MAPINFO)),]
-dropped <- dropped[,!(names(dropped)%in%c("FILE","TWAS.Z","TWAS.P"))]
+dropped <- dropped[,setdiff(names(dropped),c("FILE","TWAS.Z","TWAS.P"))]
 ajc <- merge(aj, dropped, by="Name", all=TRUE)
 ajc <- ajc[with(ajc,order(CHR,MAPINFO)),]
 write.csv(ajc,file=paste0(prefix,"ajc.csv"),quote=FALSE, row.names=FALSE)
 cat(paste0("Association + Joint/conditional annotation: ", prefix, "ajc.csv\n"))
 cat("\nThe annotation is done.\n\n")
-rm(prefix, temp, anno, annotated.data, sorted.data, sig.data, j, c, aj, ajc)
+rm(prefix, temp, included, dropped, anno, annotated.data, sorted.data, sig.data, j, c, aj, ajc)
 
 cat("Further information about FUSION and annotation is available from\n
 http://gusevlab.org/projects/fusion/
