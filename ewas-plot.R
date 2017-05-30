6f0483569c74cddb154422596aa5f25bb3374025
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-05-30 11:31:59 +0100

    EWAS-fusion plot

--- ewas-plot.R
+++ ewas-plot.R
@@ -1,5 +1,5 @@
 #!/usr/local/bin/Rscript --vanilla
-# 29-5-2017 MRC-Epid JHZ
+# 30-5-2017 MRC-Epid JHZ
 
 .libPaths("/genetics/bin/R")
 
@@ -8,32 +8,42 @@ prefix <- args[1]
 prefix <- ifelse(substring(prefix,nchar(prefix)) != "/", paste0(prefix, "/"), prefix)
 ajc_file <- paste0(prefix,"ajc.csv")
 
-use.mpfr <- FALSE
-ajc <- within(read.csv(ajc_file,as.is=TRUE),{
-  if (use.mpfr) {
-    require(Rmpfr)
-    p <- format(2*pnorm(mpfr(abs(EWAS.Z),100),lower.tail=FALSE))
-    logp <- as.numeric(-log10(mpfr(p,100)))
-    p2 <- format(2*pnorm(mpfr(abs(JOINT.Z),100),lower.tail=FALSE))
-    logp2 <- as.numeric(-log10(mpfr(p2,100)))
-    p3 <- format(2*pnorm(mpfr(abs(COND.Z),100),lower.tail=FALSE))
-    logp3 <- as.numeric(-log10(mpfr(p3,100)))
-  } else {
-    logp <- -log10(EWAS.P)
-    logp2 <--log10(JOINT.P)
-    logp3 <--log10(COND.P)
-  }
+ajc <- within(read.csv(ajc_file, as.is=TRUE), {
+  p <- p2 <- p3 <- EWAS.P
+  logP <- logp <- logp2 <- logp3 <- NA
   gene <- NA
-  color <- NA
+  colors <- NA
   ast <- "*"
+  astplus <- ""
   red <- 100
 })
+c1 <- with(ajc, !is.na(EWAS.Z))
+c2 <- with(ajc, !is.na(JOINT.Z))
+c3 <- with(ajc, !is.na(COND.Z))
+
+use.mpfr <- FALSE
+if(use.mpfr) {
+  require(Rmpfr)
+  ajc[c1, "p"] <- format(2*pnorm(mpfr(abs(ajc[c1,"EWAS.Z"]),100),lower.tail=FALSE))
+  ajc[c1, "logp"] <- as.numeric(-log10(mpfr(ajc[c1,"p"],100)))
+  ajc[c2, "p2"] <- format(2*pnorm(mpfr(abs(ajc[c2,"JOINT.Z"]),100),lower.tail=FALSE))
+  ajc[c2, "logp2"] <- as.numeric(-log10(mpfr(ajc[c2,"p2"],100)))
+  ajc[c3, "p3"] <- format(2*pnorm(mpfr(abs(ajc[c3,"COND.Z"]),100),lower.tail=FALSE))
+  ajc[c3, "logp3"] <- as.numeric(-log10(mpfr(ajc[c3,"p3"],100)))
+} else {
+  ajc[c1, "logp"] <- -log10(ajc[c1,"EWAS.P"])
+  ajc[c2, "logp2"] <- -log10(ajc[c2,"JOINT.P"])
+  ajc[c3, "logp3"] <- -log10(ajc[c3,"COND.P"])
+}
+ajc[c2, "logP"] <- ajc[c2, "logp2"]
+ajc[c3, "logP"] <- ajc[c3, "logp3"]
+ajc[c2, "astplus"] <- "j"
+ajc[c3, "astplus"] <- "c"
 
 library(dplyr)
 ajc <- subset(ajc, !is.na(logp))
-s_ajc <- summarise(group_by(ajc, CHR), m = mean(logp), sd = sd(logp), min=min(logp), max=max(logp))
 cat("\nSummary statistics for -log10(p)\n\n")
-print(s_ajc,n=22)
+print(summarise(group_by(ajc, CHR), m = mean(logp), sd = sd(logp), min=min(logp), max=max(logp)), n=22)
 cat("\n")
 library(gap)
 pdf(paste0(prefix,"ewas-plot.pdf"))
@@ -42,20 +52,19 @@ par(cex=0.6,xpd=TRUE)
 with(ajc,qqunif(EWAS.P,ci=TRUE))
 title("Association tests")
 ops <- mht.control(colors=rep(c(107,84),11),logscale=FALSE,gap=1250,srt=0,xline=1.5,yline=1.6)
-mhtplot(ajc[c("CHR","MAPINFO","logp")],ops)
+mhtdata <- ajc[c("CHR","MAPINFO","logp","gene","colors")]
+hdata <- subset(ajc, EWAS.P<=EWAS.P.Bonferroni)[c("CHR","MAPINFO","logp","ast","red")]
+dim(hdata)
+hops <- hmht.control(hdata)
+mhtplot2(mhtdata,ops,hops)
 axis(2)
 title("Association tests")
 ops <- mht.control(colors=rep(c(107,84),11),logscale=FALSE,gap=1250,srt=0,yline=2.5,xline=2)
-mhtdata <- ajc[c("CHR","MAPINFO","logp","gene","color")]
-hdata <- subset(ajc[c("CHR","MAPINFO","logp2","ast","red","JOINT.P")], !is.na(JOINT.P))
+mhtdata <- ajc[c("CHR","MAPINFO","logp","gene","colors")]
+hdata <- subset(ajc, !is.na(JOINT.P)|!is.na(COND.P))[c("CHR","MAPINFO","logP","astplus","red")]
 hops <- hmht.control(hdata)
-mhtplot2(mhtdata,ops,hops,srt=0)
+mhtplot2(mhtdata,ops,hops)
 axis(2)
-title("Association and joint(*) tests")
-hdata <- subset(ajc[c("CHR","MAPINFO","logp3","ast","red","COND.P")], !is.na(COND.P))
-hops <- hmht.control(hdata)
-mhtplot2(mhtdata,ops,hops,srt=0)
-axis(2)
-title("Association and conditional(*) tests")
+title("Association and (j)oint/(c)onditional tests")
 par(opar)
 dev.off()
