8e1fa1d76256d081d775d50903d5299ed00028a4
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-03 16:41:49 +0000

    EWAS-fusion annotation

--- ewas-annotate.R
+++ ewas-annotate.R
@@ -75,8 +75,12 @@ cat(paste0("Bonferroni-corrected significant list: ", prefix, "annotatedSortedSi
 included <- dropped <- NULL
 for (i in 1:22)
 {
-  included <- rbind(included, read.table(paste0(prefix, i, ".top.analysis.joint_included.dat"), as.is=TRUE, header=TRUE))
-  dropped <- rbind(dropped, read.table(paste0(prefix, i, ".top.analysis.joint_dropped.dat"), as.is=TRUE, header=TRUE))
+  taji <- paste0(prefix, i, ".top.analysis.joint_included.dat")
+  taji_info <- file.info(taji)
+  if (with(taji_info,size)!=0) included <- rbind(included, read.table(taji, as.is=TRUE, header=TRUE))
+  tajd <- paste0(prefix, i, ".top.analysis.joint_dropped.dat")
+  tajd_info <- file.info(tajd)
+  if (with(tajd_info,size)!=0) dropped <- rbind(dropped, read.table(tajd, as.is=TRUE, header=TRUE))
 }
 included <- rename(included, c("ID"="Name", "TWAS.Z"="EWAS.Z", "TWAS.P"="EWAS.P"))
 j <- merge(included, anno, by="Name")
