bb709f8906202cc31871c288a04afc7d2ee33e49
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-27 09:23:53 +0000

    Probe-specific genotype data

--- CpG.sh
+++ CpG.sh
@@ -2,10 +2,8 @@
 
 export p=/genetics/bin/plink-1.9
 export b=/genetics/bin/FUSION/LDREF/EUR
-export o=/scratch/tempjhz22/FUSION/plink
+export o=/scratch/tempjhz22/FUSION
 export f=500000
-
-cd /genetics/data/gwas/25-1-18
 
 function two_column_CpG_and_list_from_plink ()
 # obtain CpGID, missing data indicator, chromosome, position
@@ -20,10 +18,11 @@ cat CpG.txt | parallel --dry-run -j5 --env p --env b --env f --env o -C' ' '
    export l=$(({4}-$f)); \
    if [ $l -lt 1 ]; then export l=0; fi; \
    export u=$(({4}+$f)); \
-   $p --bfile $b --make-bed --maf 0.01 --chr {3} --from-bp $l --to-bp $u --out $o/{1}'
+   $p --bfile $b --make-bed --maf 0.01 --chr {3} --from-bp $l --to-bp $u --out $o/plink/{1}'
+   # extraction of bim files is necessary now.
 }
 
-# probe-specific data -- pending on missing value ability check
+cd /genetics/data/gwas/25-1-18
 
 awk '{
   if(NR==1) print "#chrom","Start","End","CpG"
@@ -39,29 +38,19 @@ awk '{
 }' OFS="\t" $b.bim > EUR.bed
 
 intersectBed -a CpG.bed -b EUR.bed -wa -wb > CpG.snps
-cut -f4 CpG.snps|uniq > CpG.list
-cat CpG.list | parallel -j5 -C' ' '
-  grep -w {} CpG.snps | cut -f8 > /scratch/tempjhz22/FUSION/snps/{}.snp'
+cut -f4 CpG.snps | uniq > CpG.list
+
+# probe-specific snps; 610 CpGs have no SNP data
+cat CpG.list | parallel -j5 -C' ' 'grep -w {} CpG.snps | cut -f8 > /scratch/tempjhz22/FUSION/snps/{}.snp'
+
+# PLINK data from EPIC-Omics
+cat CpG.txt | parallel --dry-run -j5 --env p --env b --env f --env o -C' ' '
+   $p --bfile /genetics/data/omics/EPICNorfolk/Axiom_UKB_EPICN_release_04Dec2014 \
+      --make-bed --extract $o/snps/{1}.snp --out $o/plink/{1}'
 
 # phenotypic and covariate data for all probes in FUSION.pheno and FUSION.covar
 #	residual data matrix with CpG ID as rownames and individual ID as colnames
 #	Inds.txt contains individual IDs which have genomic data as well
-
-function simple()
-{
-R -q --no-save <<END
-load("data/Archive/residuals")
-p1 <- mergedOut
-load("data/new/residuals")
-p2 <- mergedOut
-p <- rbind(p1,p2)
-p <- t(p)
-id <- rownames(p)
-p[is.na(p)] <- -999
-pheno <- cbind(id,as.data.frame(p))
-write.table(pheno, file="FUSION.pheno",quote=FALSE,row.names=FALSE)
-END
-}
 
 R -q --no-save <<END
 load("data/Archive/residuals")
@@ -75,21 +64,6 @@ pheno <- data.frame(id=rownames(p),p)
 omicsid <- read.table("data/Archive/Inds.txt",as.is=TRUE,col.names=c("z","id"))
 write.table(subset(pheno,id%in%with(omicsid,id)), file="FUSION.pheno",quote=FALSE,row.names=FALSE)
 END
-
-function test_in_stata()
-{
-stata <<END
-import delimited using 1., clear
-save residuals, replace
-import delimited using 2., clear
-save new, replace
-use residuals
-append using new
-xpose, clear varname
-outsheet _varname using id.dat, replace noname noquote
-outsheet using residuals.csv, comma replace noname noquote
-END
-}
 
 stata <<END
 use /genetics/data/omics/EPICNorfolk/EPICN_MDS_04Mar2015.dta
@@ -105,3 +79,37 @@ merge 1:1 omicsid using omicsid
 keep if _merge==3
 outsheet omicsid omicsid pc1-pc4 using FUSION.covar, noname noquote replace
 END
+
+## legacy code below
+
+function simple_code_for_pheno()
+{
+R -q --no-save <<END
+load("data/Archive/residuals")
+p1 <- mergedOut
+load("data/new/residuals")
+p2 <- mergedOut
+p <- rbind(p1,p2)
+p <- t(p)
+id <- rownames(p)
+p[is.na(p)] <- -999
+pheno <- cbind(id,as.data.frame(p))
+write.table(pheno, file="FUSION.pheno",quote=FALSE,row.names=FALSE)
+END
+}
+
+function stata_code_for_pheno()
+{
+stata <<END
+import delimited using 1., clear
+save residuals, replace
+import delimited using 2., clear
+save new, replace
+use residuals
+append using new
+xpose, clear varname
+outsheet _varname using id.dat, replace noname noquote
+outsheet using residuals.csv, comma replace noname noquote
+END
+}
+
