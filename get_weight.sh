60461b2234b5f8901c0a58bc2e3a2da4f11cf0fb
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-27 11:36:20 +0000

    weight generation

--- get_weight.sh
+++ get_weight.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# 26-1-2018 MRC-Epid JHZ
+# 27-1-2018 MRC-Epid JHZ
 
 export CpG=/genetics/data/twas/25-1-18/CpG.txt
 export b=/genetics/bin
-export d=/scratch2/tempjhz22/FUSION
+export d=/genetics/data/twas/25-1-18
 export o=/scratch/tempjhz22/FUSION
 export f=/genetics/bin/FUSION.compute_weights.R
 export p=/genetics/bin/plink-1.9
@@ -15,7 +15,7 @@ function get_weight_in_parallel ()
    cd $o; \
    if [ {2} -eq 1 ]; then ops="--remove $d/{1}"; fi; \
    export ops=""; \
-   $p --make-bed --missing-phenotype -999 --pheno $d.pheno --covar $d.covar --bfile $d/{1} --maf 0.01 --pheno-name {1} $ops --out {1}; \
+   $p --make-bed --missing-phenotype -999 --pheno $d.pheno --covar $d.covar --bfile $o/plink/{1} --maf 0.01 --pheno-name {1} $ops --out {1}; \
    $f --bfile={1} --tmp={1}_t --covar={1}.cov --PATH_gcta=$b/gcta_nr_robust --PATH_plink=$p --PATH_gemma=$b/gemma_1_thread \
       --crossval=0 --models=top1,lasso,enet --save_hsq --out=weights/{1}; \
    /bin/rm -f {1}.*'
