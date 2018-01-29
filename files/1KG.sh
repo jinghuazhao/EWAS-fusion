4597ef4807d2dcfe100f357e457a0e0c140f373a
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-29 13:57:10 +0000

    utilities

--- files/1KG.sh
+++ files/1KG.sh
@@ -5,23 +5,33 @@ export b=/genetics/bin/FUSION/LDREF/1000G.EUR
 export o=/scratch/tempjhz22/LDcalc/Omics
 export w=/genetics/data/twas/25-1-18
 
+# 1KG-imputed genotypes
+
+## qctool-style keep list
+
 cut -f2 data/Archive/Inds.txt > Inds.dat
+
+## obtain snpstats for all variants
 
 seq 22 | parallel -j1 --env i --env o --env w -C' ' '
     sge "/genetics/bin/qctool_v2.0 -filetype bgen -g $i/chr{}.bgen -s $i/EPICNorfolk.sample -incl-samples $w/Inds.dat -snp-stats -osnp $o/chr{}.snpstats"'
+
+## binary ped for all variants
 
 seq 22 | parallel -j1 --env i --env b --env o -C' ' '
    cut -f2 $b.{}.bim > $o/LDREF{}.snps; \
    qctool_v2.0 -filetype bgen -g $i/chr{}.bgen -s $i/EPICNorfolk.sample \
                -ofiletype binary_ped -og $o/chr{} -incl-samples Inds.dat \
                -incl-rsids $o/LDREF{}.snps -threads 5'
+## merge from all chromosomes
 
 seq 22 | awk -vo=$o '{print o "/chr" $1}' > merge-list
 plink-1.9 --merge-list merge-list --make-bed --out $o/EPIC
 
+## overlap list of variants with LDREF and binary ped
+
 export a=$o/EPIC
 export b=/genetics/bin/FUSION/LDREF/EUR
-export o=/scratch/tempjhz22/LDcalc/Omics
 
 sort -k2,2 $b.bim > LDREF.bim
 sort -k2,2 $a.bim | join -j2 - LDREF.bim | cut -d' ' -f1 > LDREF.snps
@@ -29,9 +39,12 @@ rm LDREF.bim
 
 plink-1.9 --bfile $a --extract LDREF.snps --make-bed --threads 12 --out EPIC
 
-## extract CpG SNPs via PLINK
+## variant exclusion list
 
-seq 22 | parallel -j5 --env o -C' ' 'awk "NR>12 && (\$14<0.01||\$18<0.4||\$19>=0.05){print \$2}" $o/chr{}.snpstats > $o/chr{}.exclude'
+seq 22 | parallel -j5 --env o -C' ' 'awk "NR>12 && (\$14<0.01||\$18<0.4||\$19>=0.05){print \$2}" $o/chr{}.snpstats | \
+sort -k1,1 | join -j1 - LDREF.snps > $o/chr{}.excl'
+
+## extract per CpG SNPs via PLINK
 
 export f=500000
 export o=/scratch/tempjhz22/FUSION/1KG
@@ -40,9 +53,10 @@ cat CpG.txt | parallel -j10 --env w --env f --env o -C' ' '
    if [ $l -lt 1 ]; then export l=0; fi; \
    export u=$(({4}+$f)); \
    mkdir $o/{1}; \
-   /genetics/bin/plink-1.9 --bfile EPIC --make-bed --exclude $o/chr{1}.exclude \
+   /genetics/bin/plink-1.9 --bfile EPIC --make-bed --exclude $o/chr{1}.excl \
                  --chr {3} --from-bp $l --to-bp $u --out $o/{1}/{1}'
 
+# format of snpstats
 # snpstats <- read.table("chr22.snpstats",as.is=TRUE,header=TRUE,skip=11)
 # 2 "rsid"
 # 3 chromosome
