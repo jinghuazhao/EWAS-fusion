ee2011c457bb04f6ca4a50a7ab9286bfb5f2fbef
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-09-01 16:13:16 +0100

    auxiliary files

---
+++ files/st.sh
@@ -0,0 +1,29 @@
+# 1-9-2020 JHZ
+
+git add .gitignore
+git commit -m ".gitignore"
+git add CLEAN_ZSCORES.awk
+git commit -m "strand alignment"
+git add ewas-annotate.R
+git commit -m "EWAS annotation"
+git add ewas-fusion.qsub
+git commit -m "sge"
+git add ewas-fusion.sh
+git commit -m "Bash driver"
+git add ewas-fusion.slurm
+git commit -m "SLURM"
+git add ewas-fusion.subs
+git commit -m "subroutine"
+git add ewas-plot.R
+git commit -m "EWAS plot"
+git add get_weight.qsub
+git commit -m "Generating weights"
+git add README.md README.pdf
+git commit -m "README"
+git add README.pptx
+git commit -m "PPT"
+git add files
+git commit -m "auxiliary files"
+git add test.sh
+git commit -m "test"
+git push
